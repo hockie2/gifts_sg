8282d0c524c933308a9d158fe9ae81ad65b05606
Teo Boon Hock <hockie2@yahoo.com.sg>
2019-08-20 16:16:05 +0800

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -27,12 +27,10 @@ ActiveRecord::Schema.define(version: 2019_08_20_021147) do
     t.text "description"
     t.boolean "preloved", default: true
     t.text "availability", default: "Available"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
