2eb7a12c39dec36067b54a05bd698e61cbdb10fe
Janet <cashhpvn@yahoo.com>
2019-08-22 06:06:24 +0800

    contactPage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def help
+  def contactUs
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/help'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 
   root 'items#index'
   devise_for :users  
   resources :items, :users
   get  'static_pages/about'
-  get  'static_pages/help'
+  get  'static_pages/contactUs' => 'static_pages#contactUs'
 
   
 end
