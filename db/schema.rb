74cab8142f3b8122a4cbfc04e2bfb0357bab99a6
sharkyu3 <lowhuiyu@hotmail.com>
2019-08-21 10:28:53 +0800

    adjusted schema and database

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2019_08_20_210212) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "public_id"
