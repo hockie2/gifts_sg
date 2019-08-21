dc1032dc3040af208e23940a9efa9adf09beeb8d
Janet <cashhpvn@yahoo.com>
2019-08-22 05:47:30 +0800

    created about, contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
   root 'items#index'
   devise_for :users  
   resources :items, :users
+  get  'static_pages/about'
+  get  'static_pages/help'
 
   
 end
