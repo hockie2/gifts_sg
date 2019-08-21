d59e41de8e2473c558a2054c87fe51372015017b
sharkyu3 <lowhuiyu@hotmail.com>
2019-08-21 13:26:00 +0800

    saved controller for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+class CommentsController < ApplicationController
+  def index
+    if current_user
+            @users = User.find(current_user.id)
+        end
+  end
+
+  def show
+  end
+
+  def new
+    if current_user
+        @users = User.find(current_user.id)
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+
+    @comment.item_id = params[:id]
+
+    puts "logging comment"
+    puts params[:id].to_i
+
+    @comment.save
+    redirect_to @comment
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+end
